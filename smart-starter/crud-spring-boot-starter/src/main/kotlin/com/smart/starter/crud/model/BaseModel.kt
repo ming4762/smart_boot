aeeaf7d1d6b7f9910b04d69d43f91c1c77c549b9
ming4762 <zhongming4762@hotmail.com>
2019-06-12 14:47:32 +0800

    添加crud启动器

---
+++ smart-starter/crud-spring-boot-starter/src/main/kotlin/com/smart/starter/crud/model/BaseModel.kt
@@ -0,0 +1,13 @@
+package com.smart.starter.crud.model
+
+import java.io.Serializable
+
+/**
+ * 实体类继承类
+ * 用于后续扩展
+ * @author ming
+ * 2019/6/12 上午10:36
+ */
+abstract class BaseModel : Serializable {
+
+}
