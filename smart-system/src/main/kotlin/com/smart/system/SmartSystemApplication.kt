94d5dde83460f8866e932ef9b0caaf289ceaad50
ming4762 <zhongming4762@hotmail.com>
2019-06-12 09:42:41 +0800

    初始化项目

---
+++ smart-system/src/main/kotlin/com/smart/system/SmartSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.smart.system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmartSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<SmartSystemApplication>(*args)
+}
