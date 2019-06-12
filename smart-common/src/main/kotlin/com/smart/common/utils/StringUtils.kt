17b961d44d2a893814c53c49a12e1635830dd515
ming4762 <zhongming4762@hotmail.com>
2019-06-12 14:47:02 +0800

    添加反射工具类 添加string工具类

--- smart-common/src/main/kotlin/com/smart/common/utils/StringUtils.kt
+++ smart-common/src/main/kotlin/com/smart/common/utils/StringUtils.kt
@@ -2,9 +2,11 @@ package com.smart.common.utils
 
 import org.slf4j.LoggerFactory
 import org.springframework.util.StringUtils
+import java.util.regex.Pattern
 
 /**
  * 字符串工具类
+ * TODO: 无法继承静态方法
  * @author ming
  * 2019/6/12 上午9:48
  */
@@ -22,6 +24,7 @@ object StringUtils : StringUtils() {
      * 参数{"appid": "abc123", "appsecret": "abc345"}
      * 结果 https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=abc123&secret=abc345
      */
+    @JvmStatic
     fun replaceAll(oldValue: String, newValues: Map<String, String>, ignoreCase: Boolean = false, surround: String = "#{key}"): String {
         var newValue = oldValue
         val noMatchs = mutableListOf<String>()
@@ -36,5 +39,23 @@ object StringUtils : StringUtils() {
         }
         if (noMatchs.isNotEmpty()) LOGGER.warn("以下字符匹配失败：$noMatchs")
         return newValue
+    }
+
+    /**
+     * 驼峰改为下划线
+     */
+    @JvmStatic
+    fun underscoreName(camelCaseName: String): String {
+        val pattern = Pattern.compile("[A-Z]")
+        val matcher = pattern.matcher(camelCaseName)
+        var stringBuffer = StringBuffer(camelCaseName)
+        if (matcher.find()) {
+            stringBuffer = StringBuffer()
+            matcher.appendReplacement(stringBuffer,"_"+matcher.group(0).toLowerCase())
+            matcher.appendTail(stringBuffer)
+        } else {
+            return stringBuffer.toString()
+        }
+        return underscoreName(stringBuffer.toString())
     }
 }
