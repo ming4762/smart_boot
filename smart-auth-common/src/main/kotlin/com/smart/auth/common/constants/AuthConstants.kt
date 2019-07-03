67abef8ef7936bdfdb07dfc6a72713a61dc68a3c
ming4762 <ming4762@hotmail.com>
2019-07-03 08:21:13 +0800

    切换认证常量包位置

---
+++ smart-auth-common/src/main/kotlin/com/smart/auth/common/constants/AuthConstants.kt
@@ -0,0 +1,14 @@
+package com.smart.auth.common.constants
+
+/**
+ *
+ * @author ming
+ * 2019/7/2 下午8:43
+ */
+enum class AuthConstants(value: String) {
+
+    PERMISSION("session_permission"),
+    // 登录类型
+    LOGIN_WEB_TYPE("LOGIN_WEB_TYPE"),
+    LOGIN_MOBILE_TYPE("LOGIN_MOBILE_TYPE")
+}
