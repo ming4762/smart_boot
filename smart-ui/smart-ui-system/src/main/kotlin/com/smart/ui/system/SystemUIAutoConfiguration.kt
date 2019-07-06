a3eb1f1cd98846113f9b43d99cde9e2623b75aec
ming4762 <ming4762@hotmail.com>
2019-07-06 22:01:03 +0800

    系统UI模块重构，改为模块引入

---
+++ smart-ui/smart-ui-system/src/main/kotlin/com/smart/ui/system/SystemUIAutoConfiguration.kt
@@ -0,0 +1,14 @@
+package com.smart.ui.system
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+/**
+ *
+ * @author ming
+ * 2019/7/5 下午1:58
+ */
+@Configuration
+@ComponentScan
+class SystemUIAutoConfiguration {
+}
