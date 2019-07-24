bb4cf9074931555d925b3e97bd91569f3a7f204e
ming4762 <ming4762@hotmail.com>
2019-07-24 15:49:42 +0800

    MessageMixins类添加全屏加载方法

--- smart-ui/smart-ui-common/src/main/resources/static/js/mixins/MessageMixins.js
+++ smart-ui/smart-ui-common/src/main/resources/static/js/mixins/MessageMixins.js
@@ -2,6 +2,11 @@ define(["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.default = {
+        data() {
+            return {
+                messageFullScreenloading: {}
+            };
+        },
         methods: {
             errorMessage(message, error, options) {
                 console.error(error);
@@ -17,6 +22,21 @@ define(["require", "exports"], function (require, exports) {
                 const defaultParameter = { message: message, type: 'success' };
                 const parameter = options ? Object.assign(defaultParameter, options) : defaultParameter;
                 this.$message(parameter);
+            },
+            showHideFullScreenloading(loading, options) {
+                const defaultOption = {
+                    lock: true,
+                    text: 'Loading',
+                    spinner: 'el-icon-loading',
+                    background: 'rgba(0, 0, 0, 0.7)'
+                };
+                if (loading) {
+                    this.messageFullScreenloading = this.$loading(Object.assign({}, defaultOption, options || {}));
+                    return this.messageFullScreenloading;
+                }
+                else {
+                    return this.messageFullScreenloading.close();
+                }
             }
         }
     };
--- smart-ui/smart-ui-common/src/main/resources/static/js/mixins/MessageMixins.ts
+++ smart-ui/smart-ui-common/src/main/resources/static/js/mixins/MessageMixins.ts
@@ -1,5 +1,10 @@
 
 export default {
+  data () {
+    return {
+      messageFullScreenloading: {}
+    }
+  },
   methods : {
     /**
      * 发送错误信息
@@ -28,6 +33,25 @@ export default {
       // @ts-ignore
       const parameter = options ? Object.assign(defaultParameter, options) : defaultParameter
       this.$message(parameter)
+    },
+    /**
+     * 显示打开全屏加载
+     * @param loading 加载状态
+     * @param options 参数
+     */
+    showHideFullScreenloading (loading: Boolean, options?: any): any {
+      const defaultOption = {
+        lock: true,
+        text: 'Loading',
+        spinner: 'el-icon-loading',
+        background: 'rgba(0, 0, 0, 0.7)'
+      }
+      if (loading) {
+        this.messageFullScreenloading = this.$loading(Object.assign({}, defaultOption, options || {}))
+        return this.messageFullScreenloading
+      } else {
+        return this.messageFullScreenloading.close()
+      }
     }
   }
 
