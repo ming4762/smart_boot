bab70a8afab11f1cf4d14805304184d65491f4df
ming4762 <ming4762@hotmail.com>
2019-08-15 09:18:19 +0800

    添加本地环境设置

--- smart-ui/smart-ui-common/src/main/kotlin/com/smart/ui/common/CommonAutoConfiguration.kt
+++ smart-ui/smart-ui-common/src/main/kotlin/com/smart/ui/common/CommonAutoConfiguration.kt
@@ -1,7 +1,11 @@
 package com.smart.ui.common
 
+import com.smart.ui.common.interceptor.EnvInterceptor
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 /**
  *
@@ -10,5 +14,19 @@ import org.springframework.context.annotation.Configuration
  */
 @Configuration
 @ComponentScan
-class CommonAutoConfiguration {
+class CommonAutoConfiguration : WebMvcConfigurer {
+
+    @Value("\${spring.profiles.active:}")
+    private lateinit var profiles: String
+
+    @Value("\${smart.env.development:}")
+    private lateinit var development: String
+
+    /**
+     * 添加环境设置拦截器
+     */
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(EnvInterceptor(this.profiles, this.development))
+        super.addInterceptors(registry)
+    }
 }
---
+++ smart-ui/smart-ui-common/src/main/kotlin/com/smart/ui/common/interceptor/EnvInterceptor.kt
@@ -0,0 +1,27 @@
+package com.smart.ui.common.interceptor
+
+import org.springframework.web.servlet.HandlerInterceptor
+import org.springframework.web.servlet.ModelAndView
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+/**
+ * 设置环境信息
+ * @author ming
+ * 2019/8/15 上午8:23
+ */
+class EnvInterceptor(val profiles: String, val development: String) : HandlerInterceptor {
+
+    override fun postHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any, modelAndView: ModelAndView?) {
+        if (modelAndView != null) {
+            var development = false
+            if (this.development != "") {
+                development = java.lang.Boolean.valueOf(this.development)
+            } else if (this.profiles == "pro") {
+                development = true
+            }
+            modelAndView.model["development"] = development
+        }
+        super.postHandle(request, response, handler, modelAndView)
+    }
+}
