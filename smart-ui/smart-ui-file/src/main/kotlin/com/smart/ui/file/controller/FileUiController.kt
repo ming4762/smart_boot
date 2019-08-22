7f68d2ab4dbc02d41eed4715b79333708b92d02c
ming4762 <ming4762@hotmail.com>
2019-08-22 09:13:42 +0800

    初始化ui-file包

---
+++ smart-ui/smart-ui-file/src/main/kotlin/com/smart/ui/file/controller/FileUiController.kt
@@ -0,0 +1,14 @@
+package com.smart.ui.file.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+/**
+ *
+ * @author ming
+ * 2019/8/22 上午8:56
+ */
+@Controller
+@RequestMapping("ui/file")
+class FileUiController {
+}
