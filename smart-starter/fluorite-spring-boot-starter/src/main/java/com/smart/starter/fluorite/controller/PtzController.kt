67be107e24afef1e1a189a88e93ad1a0cb5680d0
ming4762 <ming4762@hotmail.com>
2019-08-27 09:13:38 +0800

    萤石云启动模块修改接口地址

--- smart-starter/fluorite-spring-boot-starter/src/main/java/com/smart/starter/fluorite/controller/PtzController.kt
+++ smart-starter/fluorite-spring-boot-starter/src/main/java/com/smart/starter/fluorite/controller/PtzController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/hikvision/ptz")
+@RequestMapping("/fluorite/ptz")
 class PtzController {
 
     @Autowired
--- smart-starter/fluorite-spring-boot-starter/src/main/java/com/smart/starter/fluorite/controller/TokenController.kt
+++ smart-starter/fluorite-spring-boot-starter/src/main/java/com/smart/starter/fluorite/controller/TokenController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/hikvision/token")
+@RequestMapping("/fluorite/token")
 class TokenController {
 
     @Autowired
