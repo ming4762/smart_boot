69c6952a9e3d88e18fe8566b87ccfd856c543d85
ming4762 <ming4762@hotmail.com>
2019-08-30 11:22:17 +0800

    修改模块加载器 1、解决模块加载路径问题，设置项目路径后资源可能无法正常加载 2、添加layer加载模块

--- smart-ui/smart-ui-common/src/main/resources/static/js/utils/ModuleLoader.js
+++ smart-ui/smart-ui-common/src/main/resources/static/js/utils/ModuleLoader.js
@@ -9,33 +9,50 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 import CommonUtil from './CommonUtils.js';
 const moduleMap = {
     'smart-table': {
+        name: 'vue-element-table',
         js: [
-            '/js/plugins/vue-element-table/vue-element-table.umd.min.js'
+            'js/plugins/vue-element-table/vue-element-table.umd.min.js'
         ],
         css: [
-            '/js/plugins/vue-element-table/vue-element-table.css'
+            'js/plugins/vue-element-table/vue-element-table.css'
         ]
     },
     'vue-echarts': {
+        name: 'vue-echarts',
         js: [
-            '/echarts/4.2.1/echarts.min.js',
-            '/js/plugins/vue-echarts/vue-echarts.umd.min.js'
+            'echarts/4.2.1/echarts.min.js',
+            'js/plugins/vue-echarts/vue-echarts.umd.min.js'
         ]
     },
     'vue-arcgis': {
-        js: ['/js/plugins/vue-arcgis/vue-arcgis2.umd.min.js'],
-        css: ['/js/plugins/vue-arcgis/vue-arcgis2.css']
+        name: 'vue-arcgis',
+        js: ['js/plugins/vue-arcgis/vue-arcgis2.umd.min.js'],
+        css: ['js/plugins/vue-arcgis/vue-arcgis2.css']
+    },
+    layer: {
+        name: 'layer',
+        js: [
+            'jquery/3.4.1/jquery-3.4.1.min.js',
+            'layer/3.1.1/layer.js'
+        ],
     }
 };
+const getPath = CommonUtil.withContextPath;
 const loadModule = (moduleName) => {
     const module = moduleMap[moduleName];
     if (!module) {
         console.warn('模块加载失败，未找到模块');
     }
-    if (module['css'] && module['css'].length > 0) {
-        CommonUtil.loadCSS(module['css']);
+    if (window[module.name]) {
+        return new Promise(resolve => resolve(null));
     }
-    const jsList = module['js'];
+    if (module['css'] && module['css'].length > 0) {
+        const cssPaths = module['css'].map(item => {
+            return getPath(item);
+        });
+        CommonUtil.loadCSS(...cssPaths);
+    }
+    const jsList = module['js'].map(item => getPath(item));
     return CommonUtil.loadJS(...jsList);
 };
 window['smartModuleLoader'] = (...moduleNames) => __awaiter(this, void 0, void 0, function* () {
--- smart-ui/smart-ui-common/src/main/resources/static/js/utils/ModuleLoader.ts
+++ smart-ui/smart-ui-common/src/main/resources/static/js/utils/ModuleLoader.ts
@@ -3,46 +3,76 @@ import CommonUtil from './CommonUtils.js'
 
 const moduleMap = {
   'smart-table': {
+    name: 'vue-element-table',
     js: [
-        '/js/plugins/vue-element-table/vue-element-table.umd.min.js'
+        'js/plugins/vue-element-table/vue-element-table.umd.min.js'
     ],
     css: [
-        '/js/plugins/vue-element-table/vue-element-table.css'
+        'js/plugins/vue-element-table/vue-element-table.css'
     ]
   },
   'vue-echarts': {
+    name: 'vue-echarts',
     js: [
-        '/echarts/4.2.1/echarts.min.js',
-        '/js/plugins/vue-echarts/vue-echarts.umd.min.js'
+        'echarts/4.2.1/echarts.min.js',
+        'js/plugins/vue-echarts/vue-echarts.umd.min.js'
     ]
   },
   'vue-arcgis': {
-    js: ['/js/plugins/vue-arcgis/vue-arcgis2.umd.min.js'],
-    css: ['/js/plugins/vue-arcgis/vue-arcgis2.css']
+    name: 'vue-arcgis',
+    js: ['js/plugins/vue-arcgis/vue-arcgis2.umd.min.js'],
+    css: ['js/plugins/vue-arcgis/vue-arcgis2.css']
+  },
+  // 加载弹出层组件
+  layer: {
+    name: 'layer',
+    js: [
+        'jquery/3.4.1/jquery-3.4.1.min.js',
+        'layer/3.1.1/layer.js'
+    ],
   }
 }
 
+/**
+ * 获取路径
+ * @param path
+ */
+const getPath = CommonUtil.withContextPath
+
+/**
+ * 加载模块
+ * @param moduleName
+ */
 const loadModule = (moduleName: string): Promise<any> => {
   // 获取模块信息
   const module = moduleMap[moduleName]
   if (!module) {
     console.warn('模块加载失败，未找到模块')
   }
+  if (window[module.name]) {
+    // 模块已经加载
+    return new Promise<any>(resolve => resolve(null))
+  }
   // 加载css
   if (module['css'] && module['css'].length > 0) {
-    CommonUtil.loadCSS(module['css'])
+    const cssPaths = module['css'].map(item =>  {
+      return getPath(item)
+    })
+    CommonUtil.loadCSS(...cssPaths)
   }
-  // 记载js
-  const jsList = module['js']
+  // 加载js
+  const jsList = module['js'].map(item => getPath(item))
   return CommonUtil.loadJS(...jsList)
 }
 
 /**
  * 模块加载器
+ * TODO: 判断模块是否已经加载
  * @param moduleNames
  */
 window['smartModuleLoader'] = async (...moduleNames: Array<string>): Promise<any> => {
   for (let moduleName of moduleNames) {
+    // 判断是否已经加载
     await loadModule(moduleName)
   }
 }
