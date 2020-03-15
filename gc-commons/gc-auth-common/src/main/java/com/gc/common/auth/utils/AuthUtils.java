a08c259b7e8f43dfcb440fcfca321a2cf2b7776c
shizhongming <zhongming4762@hotmail.com>
2020-03-15 20:15:33 +0800

    完善获取当前登录用户功能，如果为登录直接返回null，不再抛出异常

--- gc-commons/gc-auth-common/src/main/java/com/gc/common/auth/utils/AuthUtils.java
+++ gc-commons/gc-auth-common/src/main/java/com/gc/common/auth/utils/AuthUtils.java
@@ -2,23 +2,35 @@ package com.gc.common.auth.utils;
 
 import com.gc.common.auth.model.RestUserDetails;
 import com.gc.common.auth.model.SysUserPO;
-import org.jetbrains.annotations.Nullable;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.lang.Nullable;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Objects;
 
 /**
  * 认证工具类
  * @author jackson
  * 2020/1/22 7:07 下午
  */
+@Slf4j
 public final class AuthUtils {
+
+    private static final String NOT_LOGIN_USER = "anonymousUser";
 
     /**
      * 获取当前用户
      * @return 当前用户
      */
-    @Nullable
+    @org.springframework.lang.Nullable
     public static SysUserPO getCurrentUser() {
-        return ((RestUserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUser();
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (Objects.isNull(principal) || StringUtils.equals(principal.toString(), NOT_LOGIN_USER)) {
+            log.warn("用户未登录");
+            return null;
+        }
+        return ((RestUserDetails)principal).getUser();
     }
 
     /**
