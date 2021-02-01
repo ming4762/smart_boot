f48cb9d463c513fd0886b74d98f6e2db2dab441c
shizhongming <zhongming4762@hotmail.com>
2021-02-02 00:06:32 +0800

    添加IO模块

---
+++ gc-commons/gc-commons-io/src/main/java/com/gc/commons/io/utils/YamlUtils.java
@@ -0,0 +1,13 @@
+package com.gc.commons.io.utils;
+
+/**
+ * Yaml 工具类
+ * @author shizhongming
+ * 2021/1/30 11:56 下午
+ */
+public class YamlUtils {
+
+    private YamlUtils() {
+        throw new IllegalStateException("Utility class");
+    }
+}
